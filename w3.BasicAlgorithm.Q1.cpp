2d94e625bd89b4b1943a009742893142eae31e89
duveypriyansh <77798499+duveypriyansh@users.noreply.github.com>
2021-06-21 10:57:38 +0530

    first question

---
+++ w3.BasicAlgorithm.Q1.cpp
@@ -0,0 +1,14 @@
+// w3.BasicAlgorithm.Q1.cpp
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,sum;
+    cin >> a >> b;
+    sum = a + b;
+    if(a == b) {   
+        sum = sum * 3;
+    }   
+    cout << sum;
+
+}
