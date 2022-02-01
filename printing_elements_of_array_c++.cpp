0038d334d2f87fce80c35cbc879c5dc88e9d27b3
duveypriyansh <77798499+duveypriyansh@users.noreply.github.com>
2022-02-01 06:45:25 +0530

    Add files via upload

---
+++ printing_elements_of_array_c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <array>
+int main(){
+        int i, n, arr[n];
+        cin >> n;
+        for (i = 0; i <= n; i++){
+            cin >> arr[i];
+            cout << arr[i];
+        }
+
+    return 0;
+}
