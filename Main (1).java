82d10dd8da36dce311d1b05110f7e4c8e04f7c18
duveypriyansh <77798499+duveypriyansh@users.noreply.github.com>
2022-09-07 18:14:39 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[]  args){
+        System.out.println("Enter the integer");
+        Scanner sc = new Scanner(System.in);
+        int scanned = sc.nextInt();
+        System.out.println(scanned);
+    }
+}
